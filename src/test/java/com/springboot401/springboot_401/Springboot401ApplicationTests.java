dd30a320368f14e9b88553d446db4b0d1fdf9ec9
chikebc <chikecb@outlook.com>
2020-04-01 19:51:03 -0400

    Initial commit

---
+++ src/test/java/com/springboot401/springboot_401/Springboot401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot401.springboot_401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
